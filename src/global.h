75744d9e1856d1307e6d22f565dac54ce9272536
nocisoft <norbert@nocisoft.com>
2015-08-26 14:06:18 +0200

    version fix

--- src/global.h
+++ src/global.h
@@ -2,7 +2,7 @@
 #define GLOBAL_H
 
 #define APPLICATION_NAME		"SRUP"
-#define APPLICATION_VERSION		"1.3"
+#define APPLICATION_VERSION		"1.3.1"
 #define APPLICATION_COPYRIGHT	"Copyright 2009-2015 by NociSoft/Helta Kft."
 #define APPLICATION_ABOUT		"<h3>" APPLICATION_NAME " " APPLICATION_VERSION "</h3>\n" "SQL Result Uploader Application"
 #define APPLICATION_AUTHOR 		"<h3>NociSoft Software Solutions</h3><a href=\"www.nocisoft.com\">www.nocisoft.com</a>"
